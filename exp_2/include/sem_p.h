c90201d10194186783c8f1247d5db9d58fe6f1d6
convexwf <convexwf@gmail.com>
2019-07-16 13:26:38 +0800

    format repo

--- exp_2/include/sem_p.h
+++ exp_2/include/sem_p.h
@@ -1,28 +1,28 @@
 #ifndef SEM_P_H
 #define SEM_P_H
-  
-#include <sys/types.h>  
-#include <sys/ipc.h>  
-#include <sys/sem.h>  
-#include <stdio.h>  
-  
-#define _PATH_NAME_ "/tmp"  
-#define _PROJ_ID_ 0x666  
-  
-union semun   
-{  
-    int val;  
-    struct semid_ds *buf;  
-    unsigned short  *array;  
-    struct seminfo  *__buf;   
-};  
-  
-  
-int create_sem_set(int nums);  
-int get_sem_set(int nums);  
-int init_sem_set(int sem_id, int which, int val);  
-int down(int sem_id, int num);  
-int up(int sem_id, int num);  
-int destroy(int sem_id);  
+
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <stdio.h>
+
+#define _PATH_NAME_ "/tmp"
+#define _PROJ_ID_ 0x666
+
+union semun
+{
+    int val;
+    struct semid_ds *buf;
+    unsigned short  *array;
+    struct seminfo  *__buf;
+};
+
+
+int create_sem_set(int nums);
+int get_sem_set(int nums);
+int init_sem_set(int sem_id, int which, int val);
+int down(int sem_id, int num);
+int up(int sem_id, int num);
+int destroy(int sem_id);
 
 #endif
